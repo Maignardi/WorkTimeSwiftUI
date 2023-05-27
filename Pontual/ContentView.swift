37ab14ac2e20a1afecf0bf1ec23d704fce5aa89a
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-26 22:14:49 -0400

    first commit

--- Pontual/ContentView.swift
+++ Pontual/ContentView.swift
@@ -1,88 +1,109 @@
-//
-//  ContentView.swift
-//  Pontual
-//
-//  Created by matheus maignardi on 24/05/23.
-//
-
 import SwiftUI
-import CoreData
+import Combine
+
+class TimerManager: ObservableObject {
+    @Published var isTimerRunning = false
+    @Published var remainingTime: TimeInterval = 0
+    
+    private var timer: Timer?
+    private let workingHours: TimeInterval = 9.25 * 3600 // 9 hours and 15 minutes in seconds
+    
+    func startTimer(from startTime: Date) {
+        isTimerRunning = true
+        let currentTime = Date()
+        let elapsedTime = currentTime.timeIntervalSince(startTime)
+        remainingTime = max(workingHours - elapsedTime, 0)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            
+            if self.remainingTime > 0 {
+                self.remainingTime -= 1
+            } else {
+                timer.invalidate()
+                self.isTimerRunning = false
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        isTimerRunning = false
+    }
+}
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @StateObject private var timerManager = TimerManager()
+    @State private var arrivalTime = Date()
+    
+    private var departureTime: Date {
+        let calendar = Calendar.current
+        let endTime = calendar.date(byAdding: .hour, value: 9, to: arrivalTime)!
+        return calendar.date(byAdding: .minute, value: 15, to: endTime)!
+    }
+    
+    private var formattedDepartureTime: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: departureTime)
+    }
+    
+    private var formattedRemainingTime: String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        
+        let remainingTime = TimeInterval(timerManager.remainingTime)
+        return formatter.string(from: remainingTime) ?? ""
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            Form {
+                Section(header: Text("Horário de Chegada")) {
+                    DatePicker("", selection: $arrivalTime, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(.wheel)
+                        .labelsHidden()
+                }
+                
+                Section(header: Text("Horário de Saída")) {
+                    Text(formattedDepartureTime)
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                }
+                
+                Section(header: Text("Contagem Regressiva")) {
+                    Text(formattedRemainingTime)
+                        .font(.title)
+                        .foregroundColor(.red)
+                }
+                
+                Section {
+                    Button(action: {
+                        if timerManager.isTimerRunning {
+                            timerManager.stopTimer()
+                        } else {
+                            timerManager.startTimer(from: arrivalTime)
+                        }
+                    }) {
+                        Text(timerManager.isTimerRunning ? "Stop" : "Start")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(timerManager.isTimerRunning ? Color.red : Color.blue)
+                            .cornerRadius(10)
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            .navigationBarTitle("Marcação de Ponto")
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
